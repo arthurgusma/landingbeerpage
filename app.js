4ca5221182e41b230dc1724a98eb07e13b503d4c
Arthur Gusmao <arthur.cabj@hotmail.com>
2020-09-01 15:24:47 -0300

    New: mailchimp included to the project and first set up sucessfully done.

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const request = require("request")
+const mailChimp = require("@mailchimp/mailchimp_marketing")
+
 
 const app = express()
 const port = 3000
 
+mailChimp.setConfig({
+  apiKey: "c08472b498e24018ae7b05fdecf1af4b-us17",
+  server: "us17"
+})
 
-app.use(bodyParser.urlencoded({extended: false}))
+async function callPing() {
+  const response = await mailChimp.ping.get()
+  console.log(response)
+}
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+callPing()
 
 app.use(express.static("public"))
 
@@ -15,7 +29,14 @@ app.get("/", (req, res) => {
 
 })
 
+app.post("/", (req, res) => {
+  const firstName = req.body.fname
+  const lastName = req.body.lname
+  const email = req.body.email
 
+
+
+})
 
 app.listen(port, () => {
   console.log("Server running on localhost:" + port)
