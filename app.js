c16a100ca58f629160d5609e374002102ea33b66
Arthur Gusmao <arthur.cabj@hotmail.com>
2020-09-02 22:48:19 -0300

    safe

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const request = require("request")
 const mailChimp = require("@mailchimp/mailchimp_marketing")
-
 const app = express()
-const port = 3000
+
+require("dotenv").config()
 
 mailChimp.setConfig({
-  apiKey: "c08472b498e24018ae7b05fdecf1af4b-us17",
+  apiKey: process.env.apiKey,
   server: "us17"
 })
 
@@ -30,7 +30,7 @@ app.post("/", (req, res) => {
   const firstName = req.body.fname
   const lastName = req.body.lname
   const email = req.body.email
-  const listId = "80b1739d81";
+  const listId = process.env.audienceId;
   const subscribingUser = {
     firstName: firstName,
     lastName: lastName,
@@ -59,6 +59,6 @@ app.post("/failure", (req, res) => {
   res.redirect("/")
 })
 
-app.listen(port, () => {
-  console.log("Server running on localhost:" + port)
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running")
 })
